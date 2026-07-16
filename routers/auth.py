dcc3bca9029d710003e42077cd9b5852c69ac13c
Noor-Ul-Ain16 <97411191+Noor-Ul-Ain16@users.noreply.github.com>
2026-07-17 04:05:31 +0500

    Added Login function

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from schemas import UserLogin, Token
+from auth import verify_password, create_access_token
 from database import get_db
 from models import User
 from schemas import UserCreate, UserResponse
@@ -42,3 +43,41 @@ def register(
     db.refresh(new_user)
 
     return new_user
+
+@router.post("/login", response_model=Token)
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    # Find the user by email
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    # If user doesn't exist
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    # Verify the password
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    # Create JWT token
+    access_token = create_access_token(
+        data={"user_id": db_user.id}
+    )
+
+    # Return the token
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
