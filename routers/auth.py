25d6c5ec06df38946147269d874bf41d3d3dbed4
Noor-Ul-Ain16 <97411191+Noor-Ul-Ain16@users.noreply.github.com>
2026-07-17 00:31:32 +0500

    Updated files for User Registration

--- routers/auth.py
+++ routers/auth.py
@@ -7,3 +7,28 @@ from schemas import UserCreate, UserResponse
 from auth import hash_password
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+@router.post("/register", response_model=UserResponse)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    hashed_password = hash_password(user.password)
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password,
+        role_id=user.role_id
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
